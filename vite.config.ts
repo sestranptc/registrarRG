21cae98e570e035c7a7a4942def9687a994478ae
Web RG Developer <dev@web-rg.local>
2026-02-05 16:21:44 -0300

    chore: update vite base to /registrarRG/

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/registroRg/',
+  base: '/registrarRG/',
   build: {
     sourcemap: 'hidden',
   },
